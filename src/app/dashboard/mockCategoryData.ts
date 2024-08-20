2397daeb4f129f3de210aed2b0156ffd6acf8af4
Ed <edwinli123456@gmail.com>
2024-08-21 03:54:45 +1000

    create dashboard

---
+++ src/app/dashboard/mockCategoryData.ts
@@ -0,0 +1,54 @@
+// mockCategoryData.ts
+
+import { CategoryItem } from "@/app/dashboard/CategoryGrid";
+
+export const mockCategories: CategoryItem[] = [
+  {
+    id: "1",
+    name: "10ml Luer Lock Syringe",
+    type: "SYRINGE",
+    aisle: 5,
+    column: 7,
+    image_url: "https://picsum.photos/400/300?random=1",
+  },
+  {
+    id: "2",
+    name: "Adult Igel Supraglottic",
+    type: "AIRWAY",
+    aisle: 2,
+    column: 9,
+    image_url: "https://picsum.photos/400/300?random=2",
+  },
+  {
+    id: "3",
+    name: "20ml Luer Lock Syringe",
+    type: "SYRINGE",
+    aisle: 5,
+    column: 8,
+    image_url: "https://picsum.photos/400/300?random=3",
+  },
+  {
+    id: "4",
+    name: "Pediatric Igel Supraglottic",
+    type: "AIRWAY",
+    aisle: 2,
+    column: 10,
+    image_url: "https://picsum.photos/400/300?random=4",
+  },
+  {
+    id: "5",
+    name: "5ml Luer Lock Syringe",
+    type: "SYRINGE",
+    aisle: 5,
+    column: 6,
+    image_url: "https://picsum.photos/400/300?random=5",
+  },
+  {
+    id: "6",
+    name: "Endotracheal Tube",
+    type: "AIRWAY",
+    aisle: 3,
+    column: 1,
+    image_url: "https://picsum.photos/400/300?random=6",
+  },
+];
