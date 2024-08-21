9454e16abfa500d253f38a4c4121f45d15da9b77
Ed <edwinli123456@gmail.com>
2024-08-22 04:51:23 +1000

    replace category data with real data

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "shop.medicalpantry.org",
+        port: "",
+        pathname: "/cdn/shop/files/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
