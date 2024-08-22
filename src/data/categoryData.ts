731390c02a5b7e800ffb41f8d4dd4c4a0717d6e9
ed <edwinli123456@gmail.com>
2024-08-22 11:53:34 +1000

    update images with no-background images

--- src/data/categoryData.ts
+++ src/data/categoryData.ts
@@ -8,7 +8,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 1,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/FILEcopy_990127fa-1ec9-4959-998b-444f585568e9.jpg?v=1716686899&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2FFILEcopy_990127fa-1ec9-4959-998b-444f585568e9_nobg.png?alt=media&token=c68870a1-5b3b-41dc-bc6e-37fc2386922b",
   },
   {
     id: "AIR002",
@@ -17,7 +17,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 2,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_8d27f246-ab05-4cc0-bda4-4e50c22274ab.jpg?v=1718542909&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_8d27f246-ab05-4cc0-bda4-4e50c22274ab_nobg.png?alt=media&token=eceacb00-bcbd-40f4-baf1-73d2397f09d4",
   },
   {
     id: "AIR003",
@@ -26,7 +26,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 3,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/FILEcopy_1813a71f-d194-4107-942a-fc4d4aaeb374.jpg?v=1717738768&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2FFILEcopy_1813a71f-d194-4107-942a-fc4d4aaeb374_nobg.png?alt=media&token=86d374e1-3858-4af8-862a-ea58fe39c707",
   },
   {
     id: "AIR004",
@@ -35,7 +35,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 4,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/2_890b1a5f-de65-4058-94c3-8dfd8dd26545.jpg?v=1717208979&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F2_890b1a5f-de65-4058-94c3-8dfd8dd26545_nobg.png?alt=media&token=ce690938-bf6a-469a-adb7-3befcb95158c",
   },
   {
     id: "AIR005",
@@ -44,7 +44,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 5,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_930da557-3a9c-4b05-9f39-869a423ec068.jpg?v=1718539904&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_930da557-3a9c-4b05-9f39-869a423ec068_nobg.png?alt=media&token=49359797-2384-4c43-b99b-a9e92bfc3c45",
   },
   {
     id: "AIR006",
@@ -53,7 +53,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 1,
     column: 6,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_4c5d52af-f97b-4fbb-99f7-b087f9966233.jpg?v=1717208610&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_4c5d52af-f97b-4fbb-99f7-b087f9966233_nobg.png?alt=media&token=b5fb65a2-5173-4fe2-ab69-13393a97d2ef",
   },
   {
     id: "FA001",
@@ -62,7 +62,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 1,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_bd3230ef-8a74-4c64-ae66-3bf9d904b728.jpg?v=1718597416&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_bd3230ef-8a74-4c64-ae66-3bf9d904b728_nobg.png?alt=media&token=fa913aa8-c46d-408b-aa0b-80c5c38f898a",
   },
   {
     id: "FA002",
@@ -71,7 +71,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 2,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_0f643e91-1ebb-4b8c-a7b9-820ca0a30faf.jpg?v=1718003605&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_0f643e91-1ebb-4b8c-a7b9-820ca0a30faf_nobg.png?alt=media&token=2ef1417a-9694-4d97-a825-0c20e5d58f28",
   },
   {
     id: "FA003",
@@ -80,7 +80,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 3,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_90d6f5cf-a087-46c7-9f3c-3332333d9c43.jpg?v=1718862903&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_90d6f5cf-a087-46c7-9f3c-3332333d9c43_nobg.png?alt=media&token=3ae9414d-5908-4b0c-ba0b-99873daeb800",
   },
   {
     id: "FA004",
@@ -89,7 +89,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 4,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_27177772-fd3b-492b-b2f9-27bb9df50dae.jpg?v=1718008730&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_27177772-fd3b-492b-b2f9-27bb9df50dae_nobg.png?alt=media&token=8d5f0c84-07fb-4399-a14c-0d4cce7a804d",
   },
   {
     id: "FA005",
@@ -98,7 +98,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 5,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/2_283cdf57-534f-419c-ba12-7a208e5a06f7.jpg?v=1718939283&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F2_283cdf57-534f-419c-ba12-7a208e5a06f7_nobg.png?alt=media&token=519837d1-9a88-4842-9e7c-412b198785c5",
   },
   {
     id: "FA006",
@@ -107,7 +107,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 2,
     column: 6,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_7391be9d-f3bd-43ed-899c-530cd335e27c.jpg?v=1718939458&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_7391be9d-f3bd-43ed-899c-530cd335e27c_nobg.png?alt=media&token=a003d4aa-3919-4a39-be21-bfd58a07e30c",
   },
   {
     id: "SYR001",
@@ -116,7 +116,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 3,
     column: 1,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_195457c0-13e5-43d3-9dac-ec805b3f9af3.jpg?v=1717992631&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_195457c0-13e5-43d3-9dac-ec805b3f9af3_nobg.png?alt=media&token=b8e5f9fd-525e-4703-a8a3-46f5f530a585",
   },
   {
     id: "SYR002",
@@ -125,7 +125,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 3,
     column: 2,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_195457c0-13e5-43d3-9dac-ec805b3f9af3.jpg?v=1717992631&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_195457c0-13e5-43d3-9dac-ec805b3f9af3_nobg.png?alt=media&token=b8e5f9fd-525e-4703-a8a3-46f5f530a585",
   },
   {
     id: "SYR003",
@@ -134,7 +134,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 3,
     column: 3,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/2_180e6e1f-9842-41ff-9a9f-d63092c689a6.jpg?v=1716685686&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F2_180e6e1f-9842-41ff-9a9f-d63092c689a6_nobg.png?alt=media&token=f0a56712-a46c-4761-bdbf-e70eb4df516f",
   },
   {
     id: "SYR004",
@@ -143,7 +143,7 @@ export const categoryItems: CategoryItem[] = [
     aisle: 3,
     column: 4,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/2_180e6e1f-9842-41ff-9a9f-d63092c689a6.jpg?v=1716685686&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F2_180e6e1f-9842-41ff-9a9f-d63092c689a6_nobg.png?alt=media&token=f0a56712-a46c-4761-bdbf-e70eb4df516f",
   },
   {
     id: "SYR005",
@@ -152,6 +152,6 @@ export const categoryItems: CategoryItem[] = [
     aisle: 3,
     column: 5,
     image_url:
-      "http://shop.medicalpantry.org/cdn/shop/files/1_195457c0-13e5-43d3-9dac-ec805b3f9af3.jpg?v=1717992631&width=1946",
+      "https://firebasestorage.googleapis.com/v0/b/founders-hack-2024.appspot.com/o/medical_pantry%2F1_195457c0-13e5-43d3-9dac-ec805b3f9af3_nobg.png?alt=media&token=b8e5f9fd-525e-4703-a8a3-46f5f530a585",
   },
 ];
