1fc08bf739889dfbbaf2e79688e55745aabeb879
Emily Gong <ehgong@student.unimelb.edu.au>
2024-09-20 21:29:49 +1000

    Update with comments

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,10 +36,10 @@ const LoginPage: React.FC = () => {
         // STILL NEED TO IMPLEMENT THE REDIRECTS
         switch (userData.role) {
           case "admin":
-            router.push("/dashboard"); // just goes to home for now - PROTOTYPE
+            router.push("/dashboard"); // just goes to dashboard - PROTOTYPE
             break;
           case "volunteer":
-            router.push("/dashboard"); // just goes to history for now - PROTOTYPE
+            router.push("/dashboard"); // just goes to dashboard - PROTOTYPE
             break;
           default:
             router.push("/");
