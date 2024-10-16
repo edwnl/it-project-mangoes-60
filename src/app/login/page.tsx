9d23066c95ea604846ae149eb0b77dffeba2e5cf
Ed <edwinli123456@gmail.com>
2024-10-16 23:50:19 +1100

    reworked signup and login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,26 @@
 "use client";
 
 import React, { useState } from "react";
-import { Input, Button, Form, message } from "antd";
+import { Button, Form, Input, message } from "antd";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import {
-  signInWithEmailAndPassword,
-  browserLocalPersistence,
-  setPersistence,
   browserSessionPersistence,
+  setPersistence,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "../../lib/firebaseClient";
-import Logo from "@/assets/logo_white_hole.svg";
+import { auth, db } from "@/lib/firebaseClient";
+import Logo from "@/assets/Logo.svg";
 import { withGuard } from "@/components/GuardRoute";
 
+// login page that allows users to sign in to their account
 const LoginPage: React.FC = () => {
   const [form] = Form.useForm();
   const router = useRouter();
   const [loading, setLoading] = useState(false);
 
+  // handles the form submission for signing in
   const onFinish = async (values: { email: string; password: string }) => {
     setLoading(true);
     try {
@@ -38,15 +39,11 @@ const LoginPage: React.FC = () => {
       if (userData && userData.role) {
         // Store user role in the browsers local storage
         localStorage.setItem("userRole", userData.role);
-        // makes information easily accessible so we don't need to fetch from firebase every time
-        console.log(userData.role);
-        // STILL NEED TO IMPLEMENT THE REDIRECTS
+
         switch (userData.role) {
           case "admin":
-            router.push("/categories"); // just goes to dashboard - PROTOTYPE
-            break;
           case "volunteer":
-            router.push("/"); // just goes to dashboard - PROTOTYPE
+            router.push("/");
             break;
           default:
             router.push("/login");
@@ -54,50 +51,60 @@ const LoginPage: React.FC = () => {
       } else {
         message.error("User role not found");
       }
-      // Redirect based on role
     } catch (error) {
       console.error("Error:", error);
-      message.error("Failed to sign in . Please check your credentials");
+      message.error("Failed to sign in. Please check your credentials!");
     } finally {
       setLoading(false);
     }
   };
 
-  const handleSignUp = () => {
-    router.push("/signup");
-  };
-
+  // renders the login form with input fields for email and password
   return (
-    <div className="flex items-center min-h-screen justify-center bg-white ">
+    <div className="flex items-center md:min-h-screen justify-center">
       <div className="w-full max-w-md p-16">
+        {/* renders the logo */}
         <div className="flex justify-center mb-8">
           <Image src={Logo} alt="Medical Pantry Logo" width={60} height={60} />
         </div>
 
+        {/* renders the welcome message */}
         <h1 className="text-center text-2xl font-bold ">Welcome Back!</h1>
         <p className="text-center mb-8">Please enter your details.</p>
 
-        <Form form={form} name="login" onFinish={onFinish} layout="vertical">
+        {/* renders the login form */}
+        <Form
+          size="large"
+          form={form}
+          name="login"
+          onFinish={onFinish}
+          layout="vertical"
+        >
+          {/* email input field */}
           <Form.Item
             name="email"
             label="Email"
             rules={[
-              { required: true, message: "Please input your email!" },
-              { type: "email", message: "Please enter a valid email!" },
+              { required: true, message: "Please input your email" },
+              { type: "email", message: "Please enter a valid email" },
             ]}
           >
             <Input placeholder="Enter your email" className="rounded-md h-10" />
           </Form.Item>
+
+          {/* password input field */}
           <Form.Item
             name="password"
             label="Password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            rules={[{ required: true, message: "Please input your password" }]}
           >
             <Input.Password
               placeholder="Password"
               className="rounded-md h-10"
             />
           </Form.Item>
+
+          {/* sign in button */}
           <Form.Item>
             <Button
               type="primary"
@@ -107,19 +114,14 @@ const LoginPage: React.FC = () => {
             >
               Sign In
             </Button>
-            <Button
-              onClick={handleSignUp}
-              className="w-full h-10 mt-2 custom-button"
-            >
-              Sign Up
-            </Button>
           </Form.Item>
         </Form>
-
-        <p className="text-center text-gray-600 text-sm mt-8">
-          © 2024 Medical Pantry
-        </p>
       </div>
+
+      {/* footer with copyright message */}
+      <p className="absolute bottom-5 text-gray-600 text-sm mt-8">
+        © 2024 Medical Pantry
+      </p>
     </div>
   );
 };
